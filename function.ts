d15ab897606b0e18ca76820cc797301bdb692e98
Callum Gare <callum@gare.au>
2025-09-25 19:52:47 +1000

    Fix

--- function.ts
+++ function.ts
@@ -1,11 +1,6 @@
 import * as Sentry from '@sentry/aws-serverless';
 import type { Handler } from 'aws-lambda';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-});
-
 export let handler: Handler = async function(event, context) {
     Sentry.captureMessage("This always gets reported");
     await Sentry.flush(2000);
--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,6 @@
 import { SSTConfig } from "sst";
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, OutputFormat } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
 export default {
@@ -18,7 +18,25 @@ export default {
           SENTRY_DSN: process.env.SENTRY_DSN || '',
           SENTRY_DEBUG: 'true',
           USE_SENTRY_WRAPPER: process.env.USE_SENTRY_WRAPPER || '',
-        }
+          NODE_OPTIONS: "--import ./instrument.js",
+        },
+        runtime: Runtime.NODEJS_22_X,
+        bundling: {
+          format: OutputFormat.ESM,
+          nodeModules: ['@sentry/aws-serverless'],
+          commandHooks: {
+            beforeBundling(inputDir: string, outputDir: string): string[] {
+              return [];
+            },
+            beforeInstall(): string[] {
+              return [];
+            },
+            afterBundling(inputDir: string, outputDir: string): string[] {
+              // copy extra file into output
+              return [`cp ${inputDir}/instrument.js ${inputDir}/package.json ${outputDir}`];
+            },
+          },
+        },
       });
       const fnUrl = fn.addFunctionUrl({ authType: FunctionUrlAuthType.NONE });
 
